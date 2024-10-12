62141709d4f1c717cc64176ae0f088563ae823b1
Marta Podda <martapodda@MacBook-Air-di-Marta-3.local>
2024-10-12 13:09:45 +0200

    soluzione esercizio mux

--- 1/mux/soluzioneMux.ml
+++ 1/mux/soluzioneMux.ml
@@ -11,3 +11,24 @@ let mux3 (s0 : bool) (a : bool) (b : bool) : bool =
   | (true, false, _) -> false
   | (false, _, true) -> true
   | (false, _, false) -> false;;
+
+let mux4 (s1 : bool) (s0 : bool) (a0 : bool) (a1 : bool) (a2 : bool) (a3 : bool) : bool = 
+  match (s1, s0) with 
+  | (true, true) -> a3
+  | (true, false) -> a2
+  | (false, true) -> a1
+  | (false, false) ->a0;;
+
+assert(mux4 false false false true false true = false);;
+assert(mux4 false true false true false true = true);;
+assert(mux4 true false false true false true = false);;
+assert(mux4 true true false true false true = true);;
+
+let mux (s1 : bool) (s0 : bool) (a0 : bool) (a1 : bool) (a2 : bool) (a3 : bool) : bool =
+  mux1 s1 (mux1 s0 a3 a2) (mux1 s0 a1 a0);;
+
+assert(mux false false false true false true = false);;
+assert(mux false true false true false true = true);;
+assert(mux true false false true false true = false);;
+assert(mux true true false true false true = true);;
+
